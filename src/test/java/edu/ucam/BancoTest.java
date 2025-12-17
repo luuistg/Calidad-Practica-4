992cbd33c43b86a2f9b3f25e7dd7b4c4a7a578ff
Luis Tortosa González <tortosagonzalezluis@gmail.com>
2025-12-17 19:06:01 +0100

    Resolver test

--- src/test/java/edu/ucam/BancoTest.java
+++ src/test/java/edu/ucam/BancoTest.java
@@ -48,7 +48,7 @@ class BancoTest {
 
     @Test
     void saldoTotalBanco_correcto() {
-        assertEquals(140, banco.saldoTotalBanco());
+        assertEquals(150, banco.saldoTotalBanco());
     }
 
     @Test
